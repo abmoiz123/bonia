4b23d6c358b7e01e6bcc3f692f3ff811aa381acc
Muhammad Moiz <51534906+abmoiz123@users.noreply.github.com>
2021-12-09 14:57:24 +0500

    date work done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,13 +5,62 @@ import {
     CImage
 } from '@coreui/react'
 import Img from '../assets/Set 1/1-01.png'
+import logo from '../assets/logo2.png'
 import './Home.css'
 function Home() {
-    const d = new Date();
-    // d.getMonth() + 1;
-    d.getDate();
-    d.getFullYear();
-
+    const calenderArr = [
+        {
+            id: 1,
+            date: '07',
+        },
+        {
+            id: 2,
+            date: '08',
+        },
+        {
+            id: 3,
+            date: '09',
+        },
+        {
+            id: 4,
+            date: '10',
+        },
+        {
+            id: 5,
+            date: '11',
+        },
+        {
+            id: 6,
+            date: '12',
+        },
+        {
+            id: 7,
+            date: '13',
+        },
+        {
+            id: 8,
+            date: '14',
+        },
+        {
+            id: 9,
+            date: '15',
+        },
+        {
+            id: 10,
+            date: '16',
+        },
+        {
+            id: 11,
+            date: '17',
+        },
+        {
+            id: 12,
+            date: '18',
+        },
+    ]
+    const getdate = new Date();
+    const month = getdate.toLocaleString('default', { month: 'short' });
+    const date = getdate.toLocaleString('default', { day: '2-digit' });
     return (
         // <CContainer fluid className="mainDivContainer">
         //     <CRow className="row">
@@ -29,7 +78,11 @@ function Home() {
         <div className="mainDivContainer">
             <div className="mainDivRow">
                 <div className="sideBar">
-                    {/* <CImage fluid src={Img} alt="heloo" /> */}
+                    <div className="content">
+                        <CImage fluid src={logo} alt="logo" className="logo" style={{ width: '30%' }} />
+                        <p className="scrolldown-text">SCROLL DOWN</p>
+                        <p className="sidebar-text">TO REVEAL THE DAILY SURPRISE</p>
+                    </div>
                 </div>
                 <div className="imageDiv">
                     <CImage fluid src={Img} alt="heloo" />
@@ -40,55 +93,30 @@ function Home() {
             </div>
             <div className="mainDiv2">
                 <div className="box_div">
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">{d.getDate()}</p>
-                        <p className="today">Today</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">08</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">09</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">10</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">11</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">12</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">13</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">14</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">15</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">16</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">17</p>
-                    </div>
-                    <div className="box">
-                        <p className="month">Dec</p>
-                        <p className="date">18</p>
-                    </div>
+                    {calenderArr.map(v => {
+                        return (
+                            v.date === date ? (
+                                <div key={v.id} className="activebox">
+                                    <span className="activemonth">Dec</span>
+                                    <span className="activedate">{v.date}</span>
+                                    <span className="activetoday">Today!</span>
+                                </div>
+                            ) :
+                                v.date >= date ? (
+                                    <div key={v.id} className="box">
+                                        <span className="month">Dec</span>
+                                        <span className="date">{v.date}</span>
+                                    </div>
+                                ) : (
+                                    <div key={v.id} className="pastbox">
+                                        <span className="month">Dec</span>
+                                        <span className="date">{v.date}</span>
+                                    </div>
+                                )
+                        )
+                    }
+                    )
+                    }
                 </div>
             </div>
         </div>
